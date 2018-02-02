66ce11391011bdbf357ef4f9bb31305eda5eb889
Kazuki Nara <kazuki.nara@gmail.com>
2018-02-02 11:47:20 +0900

    Format message.

--- command/help.js
+++ command/help.js
@@ -1,7 +1,7 @@
 exports.execute = function () {
     return 'jumbotの　つかいかただぞ。\n'
         + 'Usage: jumbot <command> <option>\n\n'
-        + 'げんざい　しようできる　こまんどは　これだけだぞ。\n'
+        + 'げんざい　しようできる　こまんどは　これだけだぞ。\n\n'
         + 'buki : もんはんの　ぶきを　おすすめするぞ。\n'
         + 'help : いま　つかったぞ。';
 }
