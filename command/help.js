c406e5ae785219b135a571a5635a7f56c64ed9c7
Kazuki Nara <kazuki.nara@gmail.com>
2018-02-02 11:43:31 +0900

    Add help.

---
+++ command/help.js
@@ -0,0 +1,7 @@
+exports.execute = function () {
+    return 'jumbotの　つかいかただぞ。\n'
+        + 'Usage: jumbot <command> <option>\n\n'
+        + 'げんざい　しようできる　こまんどは　これだけだぞ。\n'
+        + 'buki : もんはんの　ぶきを　おすすめするぞ。\n'
+        + 'help : いま　つかったぞ。';
+}
--- jumbot_command.js
+++ jumbot_command.js
@@ -1,11 +1,14 @@
 // models.
 var buki_roulette = require('./command/buki_roulette');
+var help = require('./command/help');
 
 exports.execute = function (username, args) {
     if (args === 'buki') {
         return buki_roulette.execute(username);
     }
+    if (args === 'help') {
+        return help.execute();
+    }
 
-
-    return args + '　のおぷしょんは　しらないぞ。'
+    return args + '　のこまんどは　しらないぞ。'
 }
