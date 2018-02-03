6f98c58dc5206e943196af8fa55bc108a1ed5421
Kazuki Nara <kazuki.nara@gmail.com>
2018-02-04 00:46:19 +0900

    Refactor main.

--- bot.js
+++ bot.js
@@ -2,33 +2,26 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 // models.
-var reaction = require('./jumbot_reaction');
-var command = require('./jumbot_command')
+var jumbot_command = require('./jumbot_command')
 
 client.on('ready', () => {
   console.log('I am ready!');
 });
 
 client.on('message', message => {
-  var channel = message.channel;
-  var author = message.author;
-  if (author.id === client.user.id) {
-    return;
-  }
-
   try {
-    var args = message.content.match(/^jumbot (.+)$/);
-    if (args != null && args.length == 2) {
-      channel.sendMessage(command.execute(author.username, args[1]));
-      return;
+    if (message.author.bot) return;
+
+    // command
+    const prefix = '!';
+    if (message.content.startsWith(prefix)) {
+      message.channel.sendMessage('テスト中。');
+      const args = message.content.slice(prefix.length).trim().split(/ +/g);
+      const command = args.shift().toLowerCase();
+
+      message.channel.sendMessage('こまんど：${command}, ぱらめーた：${args}');
     }
 
-    var generated = reaction.execute(author.username, message.content);
-    if (generated != null) {
-      channel.sendMessage(generated);
-      return;
-    }
-    
   } catch (e) {
     channel.sendMessage('えらーが　おきたぞ。\n' + e);
   }
