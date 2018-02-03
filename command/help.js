3c015777e4e1a26ba6df93a1acf1d940d75dafe5
Kazuki Nara <kazuki.nara@gmail.com>
2018-02-04 01:20:52 +0900

    Remove old files.

--- command/help.js
+++
@@ -1,8 +0,0 @@
-exports.execute = function () {
-    return 'jumbotの　つかいかただぞ。\n'
-        + 'Usage: jumbot <command> <option>\n\n'
-        + 'げんざい　しようできる　こまんどは　これだけだぞ。\n\n'
-        + 'buki : もんはんの　ぶきを　おすすめするぞ。\n'
-        + 'help : いま　つかったぞ。\n\n'
-        + 'よばれなくても　たまに　しゃべるようになったぞ';
-}
