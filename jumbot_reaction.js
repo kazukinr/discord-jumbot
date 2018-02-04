f31bbf568d614264b1cf4d0219ca8087a8dc404a
Kazuki Nara <kazuki.nara@gmail.com>
2018-02-04 10:45:55 +0900

    Add nazeda reaction. (#11)

--- jumbot_reaction.js
+++ jumbot_reaction.js
@@ -1,6 +1,7 @@
 // models.
 const iizo = require('./reaction/iizo');
 const dounaru = require('./reaction/dounaru');
+const nazeda = require('./reaction/nazeda');
 
 exports.run = function (client, message) {
 
@@ -8,5 +9,7 @@ exports.run = function (client, message) {
         iizo.run(message);
     } else if (/どうなる[。？\?]*$/.test(message.content)) {
         dounaru.run(message);
+    } else if (/なぜだ[。！？\!\?]*/.test(message.content)) {
+        nazeda.run(message);
     }
 }
