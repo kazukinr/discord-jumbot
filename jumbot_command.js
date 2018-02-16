d0f526876b2212708061a4e806a1e32f52732929
Kazuki Nara <kazuki.nara@gmail.com>
2018-02-17 00:57:58 +0900

    Add help.

--- jumbot_command.js
+++ jumbot_command.js
@@ -1,3 +1,5 @@
+const Discord = require('discord.js');
+
 // models.
 const commands = [
     require('./command/buki_roulette'),
@@ -7,12 +9,35 @@ const commands = [
 ]
 
 exports.run = function (client, message, command, args) {
+    if (command == 'help') {
+        help(message);
+        return;
+    }
+
     for (var i = 0; i < commands.length; i++) {
         if (command == commands[i].command) {
-            commands[i].run(client, message, args);
+            if (args.length == 1 && (args[0] == 'help' || args[0] == '-h')) {
+                const embed = new Discord.RichEmbed()
+                    .setTitle(command)
+                    .setDescription(commands[i].help);
+                message.channel.sendEmbed(embed);
+            } else {
+                commands[i].run(client, message, args);
+            }
             return;
         }
     }
 
     message.channel.sendMessage(command + '　のこまんどは　しらないぞ。');
+}
+
+function help(message) {
+    var embed = new Discord.RichEmbed()
+    for (var i = 0; i < commands.length; i++) {
+        embed = embed.addField(
+            commands[i].command,
+            commands[i].help
+        ).addBlankField();
+    }
+    message.channel.sendEmbed(embed);
 }
