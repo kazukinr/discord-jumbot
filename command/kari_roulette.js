1c25e6693f2b5a763cab0f044df0571a260bdcb9
Kazuki Nara <kazuki.nara@gmail.com>
2018-02-17 01:17:16 +0900

    Add help. (#31)
    * Add help.
    
    * Modify format.
    
    * Add usage.
    
    * Modify format.
    
    * format.

--- command/kari_roulette.js
+++ command/kari_roulette.js
@@ -3,9 +3,10 @@ const monster_info = require('../json/monster_info.json');
 
 exports.command = 'kari';
 
+exports.help = 'らんだむで　もんすたーを　えらぶぞ。';
+
 exports.run = function (client, message, args) {
     const keys = Object.keys(monster_info);
     const index = Math.floor(Math.random() * keys.length);
     message.channel.sendMessage(monster_info[keys[index]].name + 'を　かろう。');
 }
-
