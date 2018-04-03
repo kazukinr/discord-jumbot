0db2dedf39fc1f2f096c75faa5c3d901f8e6e998
Kazuki Nara <kazuki.nara@gmail.com>
2018-04-03 23:03:49 +0900

    Add dice option into buki. (#35)
    * Add dice option into buki.
    
    * bug fix.

--- command/buki_roulette.js
+++ command/buki_roulette.js
@@ -1,11 +1,23 @@
 // Buki Roulette.
 exports.command = 'buki';
 
-exports.help = 'らんだむで　ぶきを　えらぶぞ。';
+exports.help = '!buki : らんだむで　ぶきを　えらぶぞ。\n'
+    + '!buki <数字>d<数字>: ぶきと　だいすを　ひょうじするぞ。';
 
 exports.run = function (client, message, args) {
     const index = Math.floor(Math.random() * BUKI_LIST.length);
-    message.channel.sendMessage(message.author.username + 'は　' + BUKI_LIST[index] + 'をつかわないと　だめだぞ。');
+    let result = message.author.username + 'は　' + BUKI_LIST[index] + 'をつかわないと　だめだぞ。';
+    if (args.length == 1 && /^[0-9]+d[0-9]+$/.test(args[0])) {
+        const params = args[0].match(/^([0-9]+)d([0-9]+)$/);
+        const count = params[1];
+        const max = params[2];
+        if (count <= 100) {
+            for (var i = 0; i < count; i++) {
+                result += '\n' + (Math.ceil(Math.random() * max))
+            }
+        }
+    }
+    message.channel.sendMessage(result);
 }
 
 const BUKI_LIST = [
