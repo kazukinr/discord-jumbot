d23c5dc19579e40deaaf644f46893f89e1cc3bd8
Kazuki Nara <kazuki.nara@gmail.com>
2018-04-04 17:30:39 +0900

    Refactor dice.

--- command/buki_roulette.js
+++ command/buki_roulette.js
@@ -1,4 +1,6 @@
 // Buki Roulette.
+const dice_internal = require('../util/dice_internal');
+
 exports.command = 'buki';
 
 exports.help = '!buki : らんだむで　ぶきを　えらぶぞ。\n'
@@ -7,15 +9,9 @@ exports.help = '!buki : らんだむで　ぶきを　えらぶぞ。\n'
 exports.run = function (client, message, args) {
     const index = Math.floor(Math.random() * BUKI_LIST.length);
     let result = message.author.username + 'は　' + BUKI_LIST[index] + 'をつかわないと　だめだぞ。';
-    if (args.length == 1 && /^[0-9]+d[0-9]+$/.test(args[0])) {
-        const params = args[0].match(/^([0-9]+)d([0-9]+)$/);
-        const count = params[1];
-        const max = params[2];
-        if (count <= 100) {
-            for (var i = 0; i < count; i++) {
-                result += '\n' + (Math.ceil(Math.random() * max))
-            }
-        }
+
+    if (args.length == 1 && dice_internal.isDiceRegex(args[0])) {
+        result += '\n' + dice_internal.buildResult(args[0]);
     }
     message.channel.sendMessage(result);
 }
--- reaction/dice.js
+++ reaction/dice.js
@@ -1,26 +1,13 @@
 // Dice.
+const dice_internal = require('../util/dice_internal');
+
 exports.isTarget = function (message) {
-    return /^[0-9]+d[0-9]+$/.test(message.content);
+    return dice_internal.isDiceRegex(message.content);
 }
 
 exports.run = function (client, message) {
-    const params = message.content.match(/^([0-9]+)d([0-9]+)$/);
-    if (params.length == 3) {
-        const count = params[1];
-        const max = params[2];
-
-        if (count > 100) {
-            message.channel.sendMessage('おおすぎるぞ');
-            return;
-        }
-
-        var result = '';
-        for (var i = 0; i < count; i++) {
-            if (i > 0) {
-                result += '\n'
-            }
-            result += (Math.ceil(Math.random() * max))
-        }
-        message.channel.sendMessage(result)
+    const result = dice_internal.buildResult(message.content);
+    if (result != null && result.length > 0) {
+        message.channel.sendMessage(result);
     }
 }
