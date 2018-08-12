7d6737234c9dd1f3889b1035a1b63d6e6446d5e6
Kazuki Nara <kazuki.nara@gmail.com>
2018-08-13 01:33:05 +0900

    apply typescript.

--- reaction/eggplant.js
+++
@@ -1,9 +0,0 @@
-// Eggplant.
-exports.isTarget = function (message) {
-    return message.author.id == '409122303390253076'
-        && Math.floor(Math.random() * 100) == 0;
-}
-
-exports.run = function (client, message) {
-    message.reply('(っ’ヮ’)╮-=ﾆ= 三≡’,;’;🍆');
-}
