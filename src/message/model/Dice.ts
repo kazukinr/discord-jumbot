9b7343ee1fc87b8ff70c71d849c3b19d8cef9cea
Kazuki Nara <kazuki.nara@gmail.com>
2018-08-13 13:29:40 +0900

    fix

--- src/message/model/Dice.ts
+++ src/message/model/Dice.ts
@@ -1,31 +1,29 @@
 import { MessageHandler } from "../MessageHandler";
 import { Client, Message } from "discord.js";
+import { Random } from "../../util/Random";
 
 export class Dice implements MessageHandler {
 
     handleMessage(client: Client, message: Message): boolean {
-        // const params = message.content.match(/^([0-9]+)d([0-9]+)$/);
-        // if (params != null && params.length == 3) {
-        //     const count = parseInt(params[1]);
-        //     const max = parseInt(params[2]);
-        //     if (count > 100) {
-        //         message.channel.sendMessage("Count of dice must be same or less than 100.");
-        //         return true;
-        //     }
+        const params = message.content.match(/^([0-9]+)d([0-9]+)$/);
+        if (params != null && params.length == 3) {
+            const count = parseInt(params[1]);
+            const max = parseInt(params[2]);
+            if (count > 100) {
+                message.channel.sendMessage("Count of dice must be same or less than 100.");
+                return true;
+            }
 
-        //     var result = "";
-        //     for (var i = 0; i < count; i++) {
-        //         if (i > 0) {
-        //             result += "\n";
-        //         }
-        //         result += (Random.nextInt(max) + 1).toString();
-        //     }
-        //     message.channel.sendMessage(result);
-        //     return true;
-        // }
-        // return false;
-
-        message.channel.sendMessage("test");
-        return true;
+            var result = "";
+            for (var i = 0; i < count; i++) {
+                if (i > 0) {
+                    result += "\n";
+                }
+                result += (Random.nextInt(max) + 1).toString();
+            }
+            message.channel.sendMessage(result);
+            return true;
+        }
+        return false;
     }
 }
