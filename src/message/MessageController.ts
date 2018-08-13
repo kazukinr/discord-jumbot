167726775f0447019b15fd72d24369f69cfbf02d
Kazuki Nara <kazuki.nara@gmail.com>
2018-08-13 14:43:17 +0900

    add

--- src/message/MessageController.ts
+++ src/message/MessageController.ts
@@ -1,10 +1,12 @@
 import { Client, Message } from "discord.js";
 import { Dice } from "./model/Dice";
 import { MessageHandler } from "./MessageHandler";
+import { RocketLeagueTeam } from "./model/RocketLeagueTeam";
 
 export class MessageController {
     private readonly MODELS: MessageHandler[] = [
-        new Dice()
+        new Dice(),
+        new RocketLeagueTeam()
     ]
 
     public onMessageReceived(client: Client, message: Message) {
