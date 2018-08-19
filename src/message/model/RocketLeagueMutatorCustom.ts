ba65ad2b8acc21982d8b3ee8dedcc2e4049d5095
Kazuki Nara <kazuki.nara@gmail.com>
2018-08-19 15:39:19 +0900

    Optimize candidate of custom setting.

--- src/message/model/RocketLeagueMutatorCustom.ts
+++ src/message/model/RocketLeagueMutatorCustom.ts
@@ -7,13 +7,16 @@ import { Random } from "../../util/Random";
  * command: mc
  */
 export class RocketLeagueMutatorCustom implements MessageHandler {
-    private readonly GAME_SPEED: string[] = ["デフォルト", "スローモーション", "タイムワープ"];
+    // GAME_SPEED is currently not displayed.
+    // private readonly GAME_SPEED: string[] = ["デフォルト", "スローモーション", "タイムワープ"];
     private readonly BALL_MAX_SPEED: string[] = ["デフォルト", "低速", "高速", "超高速"];
     private readonly BALL_TYPE: string[] = ["デフォルト", "キューブ", "パック", "バスケットボール"];
     private readonly BALL_WEIGHT: string[] = ["デフォルト", "軽", "重", "超軽量", "10周年記念"];
     private readonly BALL_SIZE: string[] = ["デフォルト", "小", "大", "巨大"];
     private readonly BALL_ELASTICITY: string[] = ["デフォルト", "弱", "強", "最強"];
-    private readonly BOOST_ENERGY: string[] = ["デフォルト", "ブーストなし", "無制限", "リチャージ（低速）", "リチャージ（高速）"];
+    // non-boost is currently not selected.
+    // private readonly BOOST_ENERGY: string[] = ["デフォルト", "ブーストなし", "無制限", "リチャージ（低速）", "リチャージ（高速）"];
+    private readonly BOOST_ENERGY: string[] = ["デフォルト", "無制限", "リチャージ（低速）", "リチャージ（高速）"];
     private readonly RAMBLE: string[] = ["なし", "デフォルト", "低速", "礼儀", "デモリッション", "バネ付き", "スパイクのみ"];
     private readonly BOOST_POWER: string[] = ["1X", "1.5X", "2X", "10X"];
     private readonly GRAVITY: string[] = ["デフォルト", "低", "高", "超"];
@@ -24,7 +27,7 @@ export class RocketLeagueMutatorCustom implements MessageHandler {
         if (message.content == "mc") {
             var result = "次の試合は\n";
             result += "\n";
-            result += "ゲームスピード：" + this.GAME_SPEED[Random.nextInt(this.GAME_SPEED.length)] + "\n";
+            // result += "ゲームスピード：" + this.GAME_SPEED[Random.nextInt(this.GAME_SPEED.length)] + "\n";
             result += "ボール最高速度：" + this.BALL_MAX_SPEED[Random.nextInt(this.BALL_MAX_SPEED.length)] + "\n";
             result += "ボールタイプ　：" + this.BALL_TYPE[Random.nextInt(this.BALL_TYPE.length)] + "\n";
             result += "ボール重量　　：" + this.BALL_WEIGHT[Random.nextInt(this.BALL_WEIGHT.length)] + "\n";
